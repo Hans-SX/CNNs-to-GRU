53553d33083aa846d99cd0cec339042b395ac439
unknown <you@example.com>
2025-09-04 15:56:49 +0200

    Change to record loss every 10 batchs.

--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ def train_one_epoch(epoch_index, tb_writer, dataloader, optimizer, model, loss_f
         outputs = model(spa, ang)
 
         # Compute the loss and its gradients
-        loss = loss_func(outputs.unsqueeze(1), labels)
+        loss = loss_func(outputs, labels.reshape(-1, 1))
         loss.backward()
 
         # Adjust learning weights
@@ -23,12 +23,19 @@ def train_one_epoch(epoch_index, tb_writer, dataloader, optimizer, model, loss_f
 
         # Gather data and report
         running_loss += loss.item()
-        if i % 20 == 19:
-            last_loss = running_loss / 20 # loss per batch
+        if i % 10 == 9:
+            last_loss = running_loss / 10 # loss per batch
             print('batch {} loss: {}'.format(i + 1, last_loss))
             tb_x = epoch_index * len(dataloader) + i + 1
             # tb_writer.add_scalar('Loss/train', last_loss, tb_x)
             tb_writer.add_scalar('train/batch_loss', last_loss, tb_x)
             running_loss = 0.
+        
+        # last_loss = running_loss # loss per batch
+        # print('batch {} loss: {}'.format(i + 1, last_loss))
+        # tb_x = epoch_index * len(dataloader) + i + 1
+        # # tb_writer.add_scalar('Loss/train', last_loss, tb_x)
+        # tb_writer.add_scalar('train/batch_loss', last_loss, tb_x)
+        # running_loss = 0.
 
     return last_loss
