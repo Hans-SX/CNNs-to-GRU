e85e603bf3d9ee5bc336cefb48529f4d73bbca4a
unknown <you@example.com>
2025-09-04 15:52:44 +0200

    Adding Net_v2 and Net_v3.

--- model/network.py
+++ model/network.py
@@ -1,10 +1,11 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+from torchvision.models import resnet50
 
 
 class Net(nn.Module):
-    def __init__(self, length, width, output_size=16):
+    def __init__(self, length, width, output_size=128):
         super().__init__()
         i = 0
         re_w = width
@@ -43,14 +44,68 @@ class Net(nn.Module):
         out = self.fc_layers(out)
         return out
 
+class Net_v2(nn.Module):
+    def __init__(self, length, width, output_size=128):
+        super().__init__()
+        i = 0
+        re_w = width
+        re_l = length
+        while i < 4:
+            re_w = int(re_w / 2)
+            re_l = int(re_l / 2)
+            i += 1
+        reduced_size = re_w * re_l
+        self.cnn = nn.Sequential(
+            # Layer 1
+            nn.Conv2d(1, 8, 4, stride=2, padding=1),       # 1/2
+            nn.LeakyReLU(),
+            # Layer 2
+            nn.Conv2d(8, 16, 4, stride=2, padding=1),    # 1/2
+            nn.LeakyReLU(),
+            # Layer 3
+            nn.Conv2d(16, 32, 4, stride=2, padding=1),  # 1/2
+            nn.LeakyReLU(),
+            # Layer 4
+            nn.Conv2d(32, 64, 4, stride=2, padding=1),  # 1/2
+            nn.LeakyReLU(),
+            nn.Flatten()
+            )
+        self.fc_layers = nn.Sequential(
+            nn.Linear(reduced_size * 64, 1024),
+            nn.LeakyReLU(),
+            nn.Linear(1024, 512),
+            nn.LeakyReLU(),
+            nn.Linear(512, output_size),
+            # nn.LeakyReLU()
+            )
+
+    def forward(self, x):
+        out = self.cnn(x)
+        out = self.fc_layers(out)
+        return out
+
+
+class Net_v3(nn.Module):
+    def __init__(self, length, width, output_size=16):
+        super().__init__()
+        self.cnn = resnet50()
+        # Change input conv layer to accept 1 channel images
+        self.cnn.conv1 = nn.Conv2d(1, 64, 7, stride=2, padding=3, bias=False)
+        # Change output layer to output_size
+        self.cnn.fc = nn.Linear(2048, output_size)
+
+    def forward(self, x):
+        out = self.cnn(x)
+        return out
+    
 class ConcatenatedCNN2GRU(nn.Module):
     def __init__(self, spa_length, spa_width, ang_length, ang_width, feature_size=16, hidden_size=64, num_layers=1, sequence_length=100):
         super(ConcatenatedCNN2GRU, self).__init__()
         self.num_layers = num_layers
         self.hidden_size  = hidden_size
 
-        self.spa_cnn = Net(spa_width, spa_length, feature_size)
-        self.ang_cnn = Net(ang_width, ang_length, feature_size)
+        self.spa_cnn = Net_v3(spa_width, spa_length, feature_size)
+        self.ang_cnn = Net_v3(ang_width, ang_length, feature_size)
         self.gru = nn.GRU(2 * feature_size, hidden_size, batch_first=True)
         self.fc1 = nn.Sequential(
             nn.Linear(hidden_size, hidden_size//2),
