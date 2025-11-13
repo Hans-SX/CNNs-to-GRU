6861bdb599af339ca1acb63266dd24792d6233e6
shihminyang <ethansmin.yang@gmail.com>
2025-11-13 22:34:46 +0100

    use ConcatImg2Transformer

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 from torch.optim import Adam, lr_scheduler
 from torch.utils.tensorboard import SummaryWriter
 
-from model.network import ConcatImg2Recur
+from model.network import ConcatImg2Recur, ConcatImg2Transformer
 from dataset.HFramesSet import Hframes_Interval
 from utils import get_class, train_one_epoch
 
@@ -64,10 +64,14 @@ if __name__ == '__main__':
     h_size = config['hidden_size']
     seq_len = config['sequence_length']
     img_model = get_class(config['img_model'])
-    recur_model = get_class(config['recur_model'])
+    recur_model = get_class(config['recur_model']) if config['recur_model'] is not None else ""
 
-    # cnn2gru = ConcatenatedCNN2GRU(spa_length, spa_width, ang_length, ang_width, f_size, img_model, device, h_size, sequence_length=seq_len).to(device)
-    cnn2gru = ConcatImg2Recur(spa_length, spa_width, ang_length, ang_width, f_size, img_model, recur_model, device, h_size, sequence_length=seq_len).to(device)
+    if config.get("model_type") == "transformer":
+        cnn2gru = ConcatImg2Transformer(spa_length, spa_width, ang_length, ang_width, f_size, img_model, recur_model, device, h_size, sequence_length=seq_len).to(device)
+
+    else:
+        # cnn2gru = ConcatenatedCNN2GRU(spa_length, spa_width, ang_length, ang_width, f_size, img_model, device, h_size, sequence_length=seq_len).to(device)
+        cnn2gru = ConcatImg2Recur(spa_length, spa_width, ang_length, ang_width, f_size, img_model, recur_model, device, h_size, sequence_length=seq_len).to(device)
 
     ########################################
     # Loss function
