ca8f3e126bae26ed37df056a266d730afa8ee6c2
shihminyang <ethansmin.yang@gmail.com>
2025-11-14 11:02:36 +0100

    [feat] sample given seq_len

--- utils.py
+++ utils.py
@@ -1,19 +1,31 @@
 import importlib
+import torch
+
 
 def get_class(class_path):
     module_name, class_name = class_path.rsplit('.', 1)
     module = importlib.import_module(module_name)
     return getattr(module, class_name)
 
-def train_one_epoch(epoch_index, tb_writer, dataloader, optimizer, model, loss_func, device):
+
+def train_one_epoch(epoch_index, tb_writer, dataloader, optimizer, model,
+                    loss_func, device, seq_len=None):
     running_loss = 0.
     last_loss = 0.
 
     for i, data in enumerate(dataloader):
         # Every data instance is an input + label pair
         spa, ang, labels = data
-        spa = spa.to(device)
-        ang = ang.to(device)
+
+        if seq_len:
+            idx = torch.randperm(spa.shape[1])[:seq_len]
+            idx, _ = idx.sort()
+            spa = spa[:, idx].to(device)
+            ang = ang[:, idx].to(device)
+        else:
+            spa = spa.to(device)
+            ang = ang.to(device)
+
         labels = labels.to(device).float()
 
         # Zero your gradients for every batch!
@@ -38,7 +50,7 @@ def train_one_epoch(epoch_index, tb_writer, dataloader, optimizer, model, loss_f
             # tb_writer.add_scalar('Loss/train', last_loss, tb_x)
             tb_writer.add_scalar('train/batch_loss', last_loss, tb_x)
             running_loss = 0.
-        
+
         # last_loss = running_loss # loss per batch
         # print('batch {} loss: {}'.format(i + 1, last_loss))
         # tb_x = epoch_index * len(dataloader) + i + 1
